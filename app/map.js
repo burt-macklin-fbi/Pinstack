d9bca826eb881e9df9a5f1b746267c7ea8f60cc0
Erika Moller <erikamoller@gmail.com>
2016-12-20 10:00:40 -0600

    created XmlHttpRequest - CORS issue. Problem with localhost connecting to cross domain (Mocky). Need to implement browser plugin like Postman

--- app/map.js
+++ app/map.js
@@ -12,8 +12,6 @@ function initMap() {
     });
 
     map.addListener('click', function(e) {
-    	
-
       //inhide info box
       var info = document.getElementById('info-box');
       info.className += " active";
@@ -30,21 +28,66 @@ function initMap() {
       submit.addEventListener("click", function() { 
         name = document.getElementById('name').value;
 
-        console.log("Name: " + name + ", Lat: " + lat + ", Long: " + lng);
-      }, false);
+        console.log("Name: " + name + ", Lat: " + lat + ", Long: " + lng); 
 
-      //prepare mocky info
-      var xhr = new XMLHttpRequest();
-      xhr.open('PUT', "//www.mocky.io/v2/5185415ba171ea3a00704eed", true);
-      xhr.send();
+        //prepare mocky info
 
-      xhr.addEventListener("readystatechange", processRequest, false);
-
-      function processRequest(e) {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // time to partay!!!
+        //CORS helper method
+        function createCORSRequest(method, url) {
+          var xhr = new XMLHttpRequest();
+          if("withCredentials" in xhr) {
+            // Check if the XMLHttpRequest object has a "withCredentials" property.
+            // "withCredentials" only exists on XMLHTTPRequest2 objects.
+            xhr.open(method, url, true);
+          } else if (typeof XDomainRequest != "undefined") {
+            // Otherwise, check if XDomainRequest.
+            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+          } else {
+            // Otherwise, CORS is not supported by the browser.
+            xhr = null;
+          }
+          return xhr;
         }
-      }
+
+        // Helper method to parse the title tag from the response.
+        function getTitle(text) {
+          return text.match('<title>(.*)?</title>')[1];
+        }
+
+        // Make the actual CORS request.
+        function makeCorsRequest() {
+          // This is a sample server that supports CORS.
+          var url = 'http://www.mocky.io/v2/585858fe120000d41bc8af28';
+          var xhr = createCORSRequest('GET', url);
+          if (!xhr) {
+            alert('CORS not supported');
+            return;
+          }
+
+          //Response handlers.
+          xhr.onload = function() {
+            var text = xhr.responseText;
+            var title = getTitle(text);
+            alert('Response from CORS request to ' + url + ': ' + title);
+            
+            //Successful response
+            // if (xhr.readyState == 4 && xhr.status == 200) {
+            //   console.log(xhr.responseText);
+            //   var response = JSON.parse(xhr.responseText);
+            //   alert(response.ip);
+            // }
+          };
+          xhr.onerror = function() {
+            alert('Woops, there was an error making the request.');
+          };
+
+          xhr.send();
+        }
+
+        makeCorsRequest();
+      }, false);
 
 	}, false);
 
