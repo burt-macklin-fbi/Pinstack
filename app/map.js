8a8f87b15267efcf3478138924fd523f31449427
Erika Moller <erikamoller@gmail.com>
2017-01-21 18:13:44 -0600

    user clicks on map to create marker, submits a name, new marker is created. User clicks on marker and opens info box - shows form name submission (or Apirary test info). User can close marker. Issues - selecting outside the open info box should close all markers, should not be able to select more than one marker creation point without submitting info.

--- app/map.js
+++ app/map.js
@@ -1,14 +1,14 @@
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 6
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
     });
     // var infoWindow = new google.maps.InfoWindow({map: map});
 
     var marker = new google.maps.Marker({
-		position: map.center,
-		map: map
+    position: map.center,
+    map: map
     });
 
     map.addListener('click', function(e) {
@@ -18,113 +18,75 @@ function initMap() {
 
       //get all info
       var name;
-	    var lat = e.latLng.lat();
-	    var lng = e.latLng.lng();
+      var lat = e.latLng.lat();
+      var lng = e.latLng.lng();
 
-      //reset map to marker
-      placeMarkerAndPanTo(e.latLng, map);
+      var grabMarkerInfo = function(response) {
+        var json = JSON.parse(response);
+        console.log(json);
+
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">' + json.submitted_by + '</h1>'+
+            '<div id="bodyContent">'+
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '(last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>';
+
+        //reset map to marker
+        placeMarkerAndPanTo(e.latLng, map, contentString);
+      }
 
       var submit = document.getElementById('submit');
       submit.addEventListener("click", function() { 
         name = document.getElementById('name').value;
-
-        console.log("Name: " + name + ", Lat: " + lat + ", Long: " + lng); 
+        info.className += "info-box";
 
         //prepare mocky info
+        var request = new XMLHttpRequest();
 
-        //CORS helper method
-        function createCORSRequest(method, url) {
-          var xhr = new XMLHttpRequest();
-          if("withCredentials" in xhr) {
-            // Check if the XMLHttpRequest object has a "withCredentials" property.
-            // "withCredentials" only exists on XMLHTTPRequest2 objects.
-            xhr.open(method, url, true);
-          } else if (typeof XDomainRequest != "undefined") {
-            // Otherwise, check if XDomainRequest.
-            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-            xhr = new XDomainRequest();
-            xhr.open(method, url);
-          } else {
-            // Otherwise, CORS is not supported by the browser.
-            xhr = null;
+        request.open('POST', 'https://private-979a5-test11968.apiary-mock.com/markers');
+
+        request.setRequestHeader('Content-Type', 'application/json');
+
+        request.onreadystatechange = function () {
+          if (this.readyState === 4) {
+            // console.log('Status:', this.status);
+            // console.log('Headers:', this.getAllResponseHeaders());
+            // console.log('Body:', this.responseText);
+
+            //attach info to a marker
+            grabMarkerInfo(this.response);
           }
-          return xhr;
-        }
+        };
 
-        // Helper method to parse the title tag from the response.
-        function getTitle(text) {
-          return text.match('<title>(.*)?</title>')[1];
-        }
+        var body = {
+          'submitted_by': 'Erika Carrington',
+          'lat': 32.934292,
+          'lng': -97.078065,
+          'note': 'This is the city I live in.'
+        };
 
-        // Make the actual CORS request.
-        function makeCorsRequest() {
-          // This is a sample server that supports CORS.
-          var url = 'http://www.mocky.io/v2/585858fe120000d41bc8af28';
-          var xhr = createCORSRequest('GET', url);
-          if (!xhr) {
-            alert('CORS not supported');
-            return;
-          }
-
-          //Response handlers.
-          xhr.onload = function() {
-            var text = xhr.responseText;
-            var title = getTitle(text);
-            alert('Response from CORS request to ' + url + ': ' + title);
-            
-            //Successful response
-            // if (xhr.readyState == 4 && xhr.status == 200) {
-            //   console.log(xhr.responseText);
-            //   var response = JSON.parse(xhr.responseText);
-            //   alert(response.ip);
-            // }
-          };
-          xhr.onerror = function() {
-            alert('Woops, there was an error making the request.');
-          };
-
-          xhr.send();
-        }
-
-        makeCorsRequest();
+        request.send(JSON.stringify(body));
       }, false);
 
-	}, false);
+  }, false);
 
-
-
-  // Try HTML5 geolocation.
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(function(position) {
-  //       var pos = {
-  //         lat: position.coords.latitude,
-  //         lng: position.coords.longitude
-  //       };
-
-  //       infoWindow.setPosition(pos);
-  //       infoWindow.setContent('Location found.');
-  //       map.setCenter(pos);
-  //     }, function() {
-  //       handleLocationError(true, infoWindow, map.getCenter());
-  //     });
-  //   } else {
-  //     // Browser doesn't support Geolocation
-  //     handleLocationError(false, infoWindow, map.getCenter());
-  //   }
-  // }
-
-  // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  //   infoWindow.setPosition(pos);
-  //   infoWindow.setContent(browserHasGeolocation ?
-  //                         'Error: The Geolocation service failed.' :
-  //                         'Error: Your browser doesn\'t support geolocation.');
- }
-
-function placeMarkerAndPanTo(latLng, map) {
-	var marker = new google.maps.Marker({
-		position: latLng,
-		map: map
-	});
-map.panTo(latLng);
+  function placeMarkerAndPanTo(latLng, map, contentString) {
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+    map.panTo(latLng);
+  }
 }
 
